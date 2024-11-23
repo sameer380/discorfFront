95555c95b095fb90f8b9edcaf96435291fe9a5ec
sameer380 <73533195+sameer380@users.noreply.github.com>
2024-11-23 18:52:56 +0530

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -28,7 +28,7 @@ const Login = () => {
 			return;
 		}
 		try {
-			const response = await axios.post("http://localhost:5000/login", {
+			const response = await axios.post("https://discordback.onrender.com", {
 				email,
 				password,
 			});
